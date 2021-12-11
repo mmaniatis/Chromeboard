cf47ff53dc4e4f0f25ced513c0898fbc781d93f7
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-12-11 14:22:29 -0600

    Second initial commit.

--- chromeboard/src/whiteboard/WhiteBoard.tsx
+++ chromeboard/src/whiteboard/WhiteBoard.tsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import './WhiteBoard.css'
+
 
 class WhiteBoard extends Component {
+    canvas = {} as HTMLCanvasElement;
+    ctx = {} as any //CanvasRenderingContext2D
+
+    draw(){
+        this.ctx?.moveTo(0, 0);
+        this.ctx?.lineTo(200, 100);
+        this.ctx?.stroke();
+    }
+
+    componentDidMount() {
+        this.canvas = document.querySelector('.myCanvas') as HTMLCanvasElement;
+        this.ctx = this.canvas.getContext('2d');
+        this.draw()
+    }
+
     render() { 
         return <div id="board">
-            <input>
-            
-            </input>
-
+            <canvas className="myCanvas">
+            </canvas>
         </div>
     }
 }
