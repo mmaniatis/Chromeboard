b8a86362cab8e775795d718120b2b5ccd148a183
Michael Maniatis <mikejohnmaniatis@gmail.com>
2021-12-12 09:11:23 -0600

    #1 - Draw functionality was implemented.

--- chromeboard/src/whiteboard/WhiteBoard.tsx
+++ chromeboard/src/whiteboard/WhiteBoard.tsx
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect} from 'react';
 import './WhiteBoard.css'
-
 
 class WhiteBoard extends Component {
     canvas = {} as HTMLCanvasElement;
-    ctx = {} as any //CanvasRenderingContext2D
+    ctx = {} as any; //CanvasRenderingContext2D
+    coord = {x:0, y:0};
+    paint = false;
 
-    draw(){
-        this.ctx?.moveTo(0, 0);
-        this.ctx?.lineTo(200, 100);
-        this.ctx?.stroke();
-    }
+    constructor(props: any) {
+        super(props)
+        
+        //Bindings:
+        this.start = this.start.bind(this);
+        this.reposition = this.reposition.bind(this);
+        this.draw = this.draw.bind(this);
+        this.stop = this.stop.bind(this);
+        this.resize = this.resize.bind(this);
+    } 
 
     componentDidMount() {
         this.canvas = document.querySelector('.myCanvas') as HTMLCanvasElement;
         this.ctx = this.canvas.getContext('2d');
-        this.draw()
+        document.addEventListener("mousedown", this.start);
+        document.addEventListener("mouseup", this.stop);
+        window.addEventListener("resize", this.resize);
+    }
+
+    componentWillUnmount() {
+    } 
+    
+    resize() {
+        this.ctx.canvas.width = window.innerWidth;
+        this.ctx.canvas.height = window.innerHeight;
+    }
+
+    start(event: any) {
+        document.addEventListener("mousemove", this.draw);
+        this.reposition(event)
+    }
+
+    stop() {
+        document.removeEventListener("mousemove", this.draw);
+    }
+    
+    reposition(event: any):void {
+        this.coord.x = event.clientX - this.canvas.offsetLeft;
+        this.coord.y = event.clientY - this.canvas.offsetTop;
+    }
+
+    draw(event: any) {
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 3;
+        this.ctx.lineCap = "round";
+        this.ctx.moveTo(this.coord.x, this.coord.y);
+        this.reposition(event);
+        this.ctx.lineTo(this.coord.x, this.coord.y);
+        this.ctx.stroke();
     }
 
     render() { 
-        return <div id="board">
-            <canvas className="myCanvas">
-            </canvas>
-        </div>
+        return <canvas className="myCanvas">
+                </canvas>
+            
     }
 }
 
